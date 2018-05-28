3ded40d6817c63ad27f0d2f902a5029251fab0d5
Sam Privett <privett.sam@gmail.com>
2018-05-28 19:24:53 -0400

    Now Nodes have a default set of arcs and the minimum size of a node is set to be the same as its size hint.

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QHBoxLayout, QWidget
 
-from widgets.primitives.Arc import Arc
 from widgets.items.Node import Node
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
 
     main = QMainWindow()
+    layout = QHBoxLayout()
 
-    arcs = [Arc(thickness=30),
-            Arc(angle=45, offset=26, color=Qt.red),
-            Arc(angle=60, offset=16, color=Qt.cyan),
-            Arc(angle=80, offset=6, color=Qt.magenta),
-            Arc(angle=100, offset=-4, color=Qt.black),
-            Arc(angle=45, offset=-14, color=Qt.green),
-            Arc(angle=45, offset=-24, color=Qt.blue)]
+    layout.addWidget(Node())
+    layout.addWidget(Node())
 
-    node = Node()
-    node.addArcs(arcs)
+    widget = QWidget()
+    widget.setLayout(layout)
 
-    main.setCentralWidget(node)
+    main.setCentralWidget(widget)
     main.show()
 
     sys.exit(app.exec_())
--- widgets/items/Node.py
+++ widgets/items/Node.py
@@ -1,5 +1,7 @@
-from PyQt5.QtCore import QSize, pyqtSignal
+from PyQt5.QtCore import QSize, pyqtSignal, Qt
 from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene
+
+from widgets.primitives.Arc import Arc
 
 
 class Node(QGraphicsView):
@@ -9,15 +11,24 @@ class Node(QGraphicsView):
         super().__init__(parent)
 
         if arcs is None:
-            arcs = []
+            arcs = self.defaultArcs()
 
         self.parent = parent
         self.arcs = []
 
         self.largestArc = None
 
-        self.addArcs(arcs)
         self.setScene(QGraphicsScene(parent))
+        self.addArcs(arcs)
+
+    def defaultArcs(self):
+        return [Arc(thickness=30),
+                Arc(angle=45, offset=26, color=Qt.red),
+                Arc(angle=60, offset=16, color=Qt.cyan),
+                Arc(angle=80, offset=6, color=Qt.magenta),
+                Arc(angle=100, offset=-4, color=Qt.black),
+                Arc(angle=45, offset=-14, color=Qt.green),
+                Arc(angle=45, offset=-24, color=Qt.blue)]
 
     def mousePressEvent(self, QMouseEvent):
         super().mousePressEvent(QMouseEvent)
@@ -37,14 +48,19 @@ class Node(QGraphicsView):
     def updateLargestArc(self, arc):
         if not self.largestArc:
             self.largestArc = arc
+
         if arc.diameter + arc.thickness > self.largestArc.diameter + self.largestArc.thickness:
             self.largestArc = arc
+
+        self.updateMinimumSize()
+
+    def updateMinimumSize(self):
+        self.setMinimumSize(self.sizeHint())
 
     def addArcs(self, arcs):
         for arc in arcs:
             self.addArc(arc)
 
     def sizeHint(self):
-        height = self.largestArc.diameter + (self.largestArc.thickness + self.largestArc.offset) * 3
-        width = self.largestArc.diameter + (self.largestArc.thickness + self.largestArc.offset) * 3
-        return QSize(height, width)
+        sideLength = self.largestArc.diameter + (self.largestArc.thickness + self.largestArc.offset) * 3
+        return QSize(sideLength, sideLength)
